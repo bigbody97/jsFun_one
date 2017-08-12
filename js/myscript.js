1bd60d6dfa560a64cf485bac5e939202fa6e90ce
bigbody97 <famback17@gmail.com>
2017-08-11 22:41:18 -0700

    Function passing params.

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,36 @@
-// Comparing Booleans values
+//Basic Functions
+//Key word function, then name function "sayHi", closed "()" with no parameter being pased, then inside the "{}" is the code to be ran."console.log("Hello there people")
+
+function sayHi() {
+  console.log("Hello there pepole");
+}
+
+//This is a function that take a name parameter "name". The function has the same syntax as the function above.
+function hiFriend(name){
+  console.log("Hi my good friend"+ " " +name)
+}
+
+function upEarly(weekend, downtime) {
+  return (!weekend || downtime)
+}
+
+
+// Comparing Booleans params
 function mTrouble(aSmile, bSmile) {
-  return(!aSmile && !bSmile ) || (aSmile && bSmile)
+  return (!aSmile && !bSmile) || (aSmile && bSmile)
+}
+
+// Basic inputs and outputs
+function moreStr(str, n) {
+  // local var
+  var str = "Hello";
+  var n = 10;
+  var returnStr ="";
+  var i = 0;
+  //While loop
+  while (i<n) {
+    returnStr += str
+    itt
+  }
+  return returnStr
 }
